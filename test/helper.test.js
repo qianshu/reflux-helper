fb318d5a78ace4f4fb2feb6152350b42ef38533c
shunqian.ji <shunqian.ji@alibaba-inc.com>
2015-05-27 13:42:43 +0800

    添加子action的生成功能

--- test/helper.test.js
+++ test/helper.test.js
@@ -3,13 +3,19 @@ var path = require('path');
 var helper = require('../index');
 var fs = require('fs');
 describe('helper.test.js', function() {
-	it('test/store/store.js is exist', function() {
+	it('option is array', function() {
 		var src = path.join(__dirname, 'action/action.js');
 		var target = path.join(__dirname, 'store/store.js');
-		if (fs.existsSync(target)) {
-			fs.unlinkSync(target);
-		}
 		helper.createStore(src, target);
-		fs.existsSync(target);
+		fs.existsSync(target).should.equal(true);
+    fs.unlinkSync(target);
+	});
+
+	it('option has childrend', function() {
+		var src = path.join(__dirname, 'action/action_child.js');
+		var target = path.join(__dirname, 'store/store_child.js');
+		helper.createStore(src, target);
+		fs.existsSync(target).should.equal(true);
+    fs.unlinkSync(target);
 	});
 });
